801853eb168fd1e917a296fbac88f622f5804ab8
litiancheng <13638674083@163.com>
2020-06-15 20:43:27 +0800

    app.vue完成了首页

--- src/main.js
+++ src/main.js
@@ -2,7 +2,9 @@ import Vue from 'vue'
 import App from './App.vue'
 
 Vue.config.productionTip = false
-
+// 导入 bootstrap.css 和index.css
+import '../node_modules/bootstrap/dist/css/bootstrap.css'
+import './assets/css/index.css'
 new Vue({
   render: h => h(App),
 }).$mount('#app')

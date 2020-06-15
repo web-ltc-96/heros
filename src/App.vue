8b0a5631adeee0dd919aa9b7c04128fee544892e
litiancheng <13638674083@163.com>
2020-06-15 21:09:37 +0800

    提取公共组件AppHeader.vue文件

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,8 @@
 <template>
   <div id="app">
-     <nav class="navbar navbar-inverse navbar-fixed-top">
-      <div class="container-fluid">
-        <div class="navbar-header">
-          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-          <a class="navbar-brand" href="#">CRUD</a>
-        </div>
-      </div>
-    </nav>
+    <!-- 导航头部 -->
+    <app-header></app-header>
+   
     <div class="container-fluid">
       <div class="row">
         <div class="col-sm-3 col-md-2 sidebar">
@@ -241,12 +231,14 @@
 </template>
 
 <script>
-
+// 导入APPHeader组件
+import AppHeader from './components/APPHeader.vue'
 
 export default {
   name: 'App',
+  //注册组件
   components: {
-    
+   AppHeader
   }
 }
 </script>

bef18ab7ffe5e9c966b7fc65a75c17577c10eaf4
litiancheng <13638674083@163.com>
2020-06-15 21:58:15 +0800

    配置路由对象

--- src/main.js
+++ src/main.js
@@ -1,10 +1,22 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue' //导入vue
+import App from './App.vue' // 导入视图
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false // 不知道什么意思？？？
+//导入vue-router模块
+import VueRouter from 'vue-router'
+//注册到Vue中
+Vue.use(VueRouter)
+// 创建路由对象
+const router = new VueRouter({
+  routes:[
+    
+  ]
+}) 
 // 导入 bootstrap.css 和index.css
 import '../node_modules/bootstrap/dist/css/bootstrap.css'
 import './assets/css/index.css'
-new Vue({
+ new Vue({
   render: h => h(App),
+  // 配置路由对象，挂载到vue实例中
+ router
 }).$mount('#app')
